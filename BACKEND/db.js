56931cd8cdcce5ffa84dd459b870ff21e327f203
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-04-07 14:21:24 -0300

    .

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -3,10 +3,10 @@ require("dotenv").config(); // Carga las variables de .env
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user: 'root',
+    password: 'Saladillo2521',
+    database: 'clientesdb'
 });
 
 connection.connect((err) => {
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./db");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
@@ -9,6 +10,10 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Servir archivos estáticos desde la carpeta 'public'
+const publicPath = path.join(__dirname, "..", "FRONTEND", "public");
+app.use(express.static(publicPath));
 
 // Ruta para recibir los datos del formulario
 app.post("/guardar", (req, res) => {
