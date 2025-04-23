da1f589d3d48c5c8d854454af0ed7b96216202d1
Pedro Marcenaro <pmarcenaro@fi.uba.ar>
2025-04-23 14:37:13 +0200

    posible solucion del port

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,7 +7,7 @@ const supabase = require("./supabase");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
